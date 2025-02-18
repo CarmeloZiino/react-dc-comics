ba06226b574d013733bca489233da3e7d1a48123
“CarmeloZiino” <carmeloziino.dev@gmail.com>
2025-02-18 15:16:58 +0100

    Add: Repo + Header

---
+++ Dc-Comic-React/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./index.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
+function App() {
+
+  return (
+   <Header/>
+  );
+}
+
+export default App;
---
+++ Dc-Comic-React/src/components/Header.jsx
@@ -0,0 +1,89 @@
+const Header = () => {
+  return (
+    <header className="container p-4">
+      <div className="row align-items-center justify-content-between">
+        <figure className="col-6">
+          <img src="./src/assets/img/dc-logo.png" alt="" />
+        </figure>
+
+        <nav className="col-6">
+          <ul className="d-flex flex-col gap-4 list-unstyled">
+            <li className="">
+              <a
+                className="fw-bold text-dark text-uppercase text-decoration-none"
+                href="#"
+              >
+                Characters
+              </a>
+            </li>
+            <li className="">
+              <a
+                className="fw-bold text-dark text-uppercase text-decoration-none"
+                href="#"
+              >
+                Movies
+              </a>
+            </li>
+            <li className="">
+              <a
+                className="fw-bold text-dark text-uppercase text-decoration-none"
+                href="#"
+              >
+                Tv
+              </a>
+            </li>
+            <li className="">
+              <a
+                className="fw-bold text-dark text-uppercase text-decoration-none"
+                href="#"
+              >
+                Games
+              </a>
+            </li>
+            <li className="">
+              <a
+                className="fw-bold text-dark text-uppercase text-decoration-none"
+                href="#"
+              >
+                Collectibles
+              </a>
+            </li>
+            <li className="">
+              <a
+                className="fw-bold text-dark text-uppercase text-decoration-none"
+                href="#"
+              >
+                videos
+              </a>
+            </li>
+            <li className="">
+              <a
+                className="fw-bold text-dark text-uppercase text-decoration-none"
+                href="#"
+              >
+                fans
+              </a>
+            </li>
+            <li className="">
+              <a
+                className="fw-bold text-dark text-uppercase text-decoration-none"
+                href="#"
+              >
+                news
+              </a>
+            </li>
+            <li className="">
+              <a
+                className="fw-bold text-dark text-uppercase text-decoration-none"
+                href="#"
+              >
+                shop
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+export default Header
